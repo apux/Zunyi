3a8be5144d96c1c457bb443f58b1faebe41ee06b
Azarel Doroteo Pacheco <azarel.doroteo@logicalbricks.com>
2012-05-06 11:19:41 -0500

    se crea el proyecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zunyi::Application.config.secret_token = 'f16229dedcabed6a84cb24e163cc21ecef99d551c289397c6fb67f620e4d419e9bb53fdf3194cc8b5d0b222cb02c47035c1519bc658a52a2be2b24cf27956f60'
